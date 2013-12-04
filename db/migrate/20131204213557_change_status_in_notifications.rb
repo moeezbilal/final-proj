41f9720a011c760ba96e9423d1410032f663a16a
noumanawais <miannomi123@gmail.com>
2013-12-04 16:52:07 -0500

    newdatabase

--- db/migrate/20131204213557_change_status_in_notifications.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStatusInNotifications < ActiveRecord::Migration
-  def up
-change_column :notifications, :status, :integer
-  end
-
-  def down
-change_column :notifications, :status, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131204213557) do
+ActiveRecord::Schema.define(:version => 20131126161405) do
 
   create_table "authorizations", :force => true do |t|
     t.integer  "user_id"
